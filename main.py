ffcad43e2ed0bdad354bf94aec021ce68a33de0d
bbenefield89 <bsquared18@gmail.com>
2018-08-26 15:57:28 -0400

    CREATE `call_notification()` FUNCTION
    - in main.py
    call `subprocess.call()` method to send desktop notifications on LINUX based platforms
    more research needs to be done to first find what OS the user is on and how to make calls to their specific notification API

--- main.py
+++ main.py
@@ -1,3 +1,4 @@
+import subprocess
 import time
 
 alarm_time   = input('\nWhat time: ')
@@ -27,7 +28,14 @@ def set_alarm(seconds):
     
     
 ##
-#does_user_agree
+# call_notification
+#
+def call_notification():
+    subprocess.run(['notify-send', 'SOUND ALARM!'])
+
+
+##
+# does_user_agree
 #
 def does_user_agree(seconds):
     alarm_accepted = check_if_correct()
@@ -38,33 +46,33 @@ def does_user_agree(seconds):
         
         print(f'\nThe alarm has been set and will go off at {time.ctime(seconds)}\n')
         set_alarm(alarm_time)
-        print('\nSOUND ALARM!\n\n')
+        call_notification()
     elif alarm_accepted == 'n':
         print(f'\nSET TIME TO PM\n\n')
 
 
-if int(mk_time * 1000) <= int(time.time() * 1000):
-    print(f'\nWRONG TIME 1 {time.ctime(mk_time)}\n\n')
-    # use the same values but for PM
-    p_alarm_time = time.strptime(f'{curr_time[1]} {curr_time[2]} {curr_time[0]} {alarm_time}pm', '%m %d %Y %I:%M%p')
-    mk_time      = time.mktime(p_alarm_time)
-
+##
+# main
+#
+if __name__ == '__main__':
     if int(mk_time * 1000) <= int(time.time() * 1000):
-        print(f'\nWRONG TIME 2 {time.ctime(mk_time)}\n\n')
-        # use the original values in the AM but for the next day: curr_time[2] + 1
-        p_alarm_time = time.strptime(f'{curr_time[1]} {curr_time[2] + 1} {curr_time[0]} {alarm_time}pm', '%m %d %Y %I:%M%p')
+        # use the same values but for PM
+        p_alarm_time = time.strptime(f'{curr_time[1]} {curr_time[2]} {curr_time[0]} {alarm_time}pm', '%m %d %Y %I:%M%p')
         mk_time      = time.mktime(p_alarm_time)
 
-        print(f'\nThe alarm is set to go off at {time.ctime(mk_time)}\n\n')
+        if int(mk_time * 1000) <= int(time.time() * 1000):
+            # use the original values in the AM but for the next day: curr_time[2] + 1
+            p_alarm_time = time.strptime(f'{curr_time[1]} {curr_time[2] + 1} {curr_time[0]} {alarm_time}pm', '%m %d %Y %I:%M%p')
+            mk_time      = time.mktime(p_alarm_time)
 
-        does_user_agree(mk_time)
+            print(f'\nThe alarm is set to go off at {time.ctime(mk_time)}\n\n')
 
-    # if setting alarm to PM allowed
-    # check if user is okay with this time
-    # e.g. August 25 2018 4:40pm
+            does_user_agree(mk_time)
+
+        # if setting alarm to PM allowed
+        # check if user is okay with this time
+        # e.g. August 25 2018 4:40pm
+        else:
+            does_user_agree(mk_time)
     else:
-        # print(f'\n\nCHECK IF THIS IS RIGHT 2\n\n')
         does_user_agree(mk_time)
-
-else:
-    print(f'\nCHECK IF THIS IS RIGHT 1\n\n')
