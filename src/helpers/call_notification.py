8db463eb0acb186b18828827aa3407e250093824
bbenefield89 <bsquared18@gmail.com>
2018-08-29 01:34:58 -0400

    MOVE SYSTEM CALLS
    - in src/helpers/call_notification.py

--- src/helpers/call_notification.py
+++ src/helpers/call_notification.py
@@ -4,7 +4,7 @@ import subprocess
 # call_notification
 #
 def call_notification():
-    # plays .oga sound
-    subprocess.run(['paplay', '/usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga'])
     # calls the `notify-send` system call
     subprocess.run(['notify-send', 'Pylarm Alarm'])
+    # plays .oga sound
+    subprocess.run(['paplay', '/usr/share/sounds/freedesktop/stereo/alarm-clock-elapsed.oga'])
