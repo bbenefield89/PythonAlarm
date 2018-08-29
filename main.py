872de94219c82ff096ca157419386fdd81f21676
bbenefield89 <bsquared18@gmail.com>
2018-08-28 23:58:31 -0400

    CREATE `manually_set_alarm()`
    - in main.py
    when the program cant figure it a time based on user input allow the user to manually enter a date and time
    format: MM DD YYYY HH:MM(am/pm)
    example: 08 03 2019 11:30pm

--- main.py
+++ main.py
@@ -1,17 +1,12 @@
 import subprocess
 import time
 
-alarm_time   = input('\nWhat time: ')
-curr_time    = time.localtime()
-# alarm will attempt to set this for AM
-p_alarm_time = time.strptime(f'{curr_time[1]} {curr_time[2]} {curr_time[0]} {alarm_time}am', '%m %d %Y %I:%M%p')
-mk_time      = time.mktime(p_alarm_time)
 
 ##
 # check_if_correct:
-#   reusable while loop to check if alarm time is correct
 #
 def check_if_correct():
+    ''' reusable while loop to check if alarm time is correct '''
     acceptable = False
     while not acceptable:
         alarm_accepted = input('Is this correct [Y/N]: ').lower()
@@ -35,6 +30,25 @@ def call_notification():
 
 
 ##
+# manually_set_alarm
+#
+def manually_set_alarm():
+    ''' let the user manually set the alarm '''
+    # have user manually set their alarm
+    alarm_time = input('Manually set the alarm: MM DD YYYY HH:MM(am/pm)')
+    # parse the alarm string into datetime tuple
+    p_alarm_time = time.strptime(alarm_time, '%m %d %Y %I:%M%p')
+    # parse datetime tuple into integer of seconds
+    mk_time = time.mktime(p_alarm_time)
+    # get time when alarm should go off
+    seconds_until_alarm = int(mk_time * 1) - int(time.time() * 1)
+
+    print(f'\nThe alarm has been set and will go off at {time.ctime(mk_time)}\n')
+    set_alarm(seconds_until_alarm)
+    call_notification()
+
+
+##
 # does_user_agree
 #
 def does_user_agree(seconds):
@@ -48,13 +62,20 @@ def does_user_agree(seconds):
         set_alarm(alarm_time)
         call_notification()
     elif alarm_accepted == 'n':
-        print(f'\nSET TIME TO PM\n\n')
-
+        # print('\n\nSET ALARM TO PM\n\n')
+        manually_set_alarm()
+        
 
 ##
 # main
 #
 if __name__ == '__main__':
+    alarm_time   = input('\nWhat time: ')
+    curr_time    = time.localtime()
+    # alarm will attempt to set this for AM
+    p_alarm_time = time.strptime(f'{curr_time[1]} {curr_time[2]} {curr_time[0]} {alarm_time}am', '%m %d %Y %I:%M%p')
+    mk_time      = time.mktime(p_alarm_time)
+    
     if int(mk_time * 1000) <= int(time.time() * 1000):
         # use the same values but for PM
         p_alarm_time = time.strptime(f'{curr_time[1]} {curr_time[2]} {curr_time[0]} {alarm_time}pm', '%m %d %Y %I:%M%p')
