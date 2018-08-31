9fe18e0ccdc0963d52232ceff35f95ca003899b7
bbenefield89 <bsquared18@gmail.com>
2018-08-31 19:22:27 -0400

    CLEAN UP CLI CONDITIONALS
    - in src/main.py

--- src/main.py
+++ src/main.py
@@ -3,6 +3,7 @@ if __name__ == '__main__':
     import json
     
     from pylarm import pylarm
+    from helpers.print_cli_options_error import print_cli_options_error
     
     
     # opens and converts `cli_options.json` into a dictionary
@@ -10,22 +11,15 @@ if __name__ == '__main__':
         arguments = json.load(data)
 
 
-    def print_cli_options_error(cli_args):
-        if len(cli_args) == 2:
-            print(f'\npylarm: option {cli_args[1]}: unexpected option')
-            print('\npylarm: try \'pylarm --help\' or \'pylarm --manual\' for more information\n')
-        elif len(cli_args) == 3:
-            print(f'\npylarm: option {cli_args[1]} {cli_args[2]}: unexpected options')
-            print('\npylarm: try \'pylarm --help\' or \'pylarm --manual\' for more information\n')
-
-
     if len(sys.argv) == 1:
         pylarm()
+        
     elif len(sys.argv) == 2:
-        if hasattr(arguments[ '2' ], sys.argv[1]) == False:
-            print_cli_options_error(sys.argv)
-        else:
+        try:
             print(arguments[ '2' ][ sys.argv[1] ][ 'info' ])
+        except:
+            print_cli_options_error(sys.argv)
+            
     elif len(sys.argv) == 3:
         try:
             print(arguments[ '3' ][ sys.argv[2] ][ sys.argv[1] ][ 'info' ])
