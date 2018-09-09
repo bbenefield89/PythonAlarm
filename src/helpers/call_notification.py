1c5745ff5c3c21959ea9ef65d8b184be8e741843
Nikhil Kamineni <nikhil.kamineni@gmail.com>
2018-09-08 20:05:14 -0700

    Add support for macOS notifications
    modified:   src/helpers/call_notification.py
    - Add subprocess call for native macOS notification
    - Add subprocess call to play .aiff file with `afplay`
    - Update docstrings
    - Minor formatting
    
    new file:   src/sounds/alarm-clock-elapsed.aiff
    - Add .aiff format of `alarm-clock-elapsed` file for macOS

--- src/helpers/call_notification.py
+++ src/helpers/call_notification.py
@@ -1,7 +1,5 @@
-import os
 import platform
 import subprocess
-import sys
 
 ##
 # call_notification
@@ -10,9 +8,15 @@ def call_notification(path):
     '''
     @summary    Run OS subprocesses
 
-    @desc       When Pylarm ends it will call these two OS subprocesses. "notify-send" sneds a
-                notification to a linux based OS. "paplay" will play the specified ".oga" sound
-                file.
+    @desc       When Pylarm ends it will call these two OS subprocesses.
+
+                Linux:
+                    "notify-send" sends a notification to a linux based OS.
+                    "paplay" will play the specified ".oga" sound file.
+                macOS:
+                    "osascript" uses applescript to send a notification using
+                    native macOS notifications API.
+                    "afplay" will play the specified ".aiff" sound file.
 
     @author     Brandon Benefield
     @since      v1.0.0
@@ -20,10 +24,8 @@ def call_notification(path):
     @param      {void}
     @return     {void}
     '''
-
     if platform.system() == 'Linux':
         alarm_sound_path = path + '/sounds/alarm-clock-elapsed.oga'
-
         # calls the `notify-send` system call
         subprocess.run(['notify-send', 'Pylarm Alarm'])
         # plays .oga sound
@@ -32,8 +34,18 @@ def call_notification(path):
     elif platform.system() == 'Windows':
         print('\n\nNEED SUPPORT FOR WINDOWS\n\n')
 
-    elif platform.system() == 'Darwin':
-        print('\n\nNEED SUPPORT FOR MAC\n\n')
+    elif platform.system() == 'Darwin':  # aka 'macOS/ OSX'
+        # Use .aiff files for macOS. `afplay` system call does not support .oga
+        alarm_sound_path = path + '/sounds/alarm-clock-elapsed.aiff'
+
+        # Calls native macOS natification using applescript
+        subprocess.run([
+            'osascript',
+            '-e display notification \"Your alarm went off!\" with title \"Pylarm\"'
+        ])
+
+        # plays .aiff sound
+        subprocess.run(['afplay', alarm_sound_path])
 
     else:
         print('\n\nUNSUPPORTED OPERTING SYSTEM\n\n')
