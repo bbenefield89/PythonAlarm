9ad4c95470a41923f0827c4dd315568df5716bc3
williamchaseling <ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDzRccaHCXyKxoMwVr++5PuaY2weyPoBKI8bydsFJqH7W10uLuIDgCyi8SB6iSfVh03MNQ1nMXCkm3X/RoOW5WTFtWf4X/nbfofMuQwSftdbVUSo8FKvXFvzYEY7gfcgO893YCHNoD6tKaEB2SYEkc1cbfxj9lKfDGh3k7yx7NU1SOwI4nEc68hQ+SmUjtBKfLGi98dD/XaLxt6NLnTqtUEKCULaMd2A1uCD2Sa2WckZbCoLz9DomtLNFQFzgIahdz4TtB4HVI15GRosOSCI4zNU2+b+t6QPcI3SRAvCvriqrfzb2rceSJVx+L2Hcq/owj9nIX9VJDkPXn8tUW/mvFdZId2T1YbIF24DKY5V6UguJCU6oEFTIc1K/o6erSEC7U3eG1KFH06LMUG2Oh2Q+oatsH4690HEiRvQP4AGnDwATKOZAxO3J+AD0mOpmAb374y363hn6eqMArYwul2Afi2aVm9hUAW4EuZ7TtqX8yEa6hlAdDRFRJhB+0PBMQLOEoaIvpSBiU7Bw3Mqf8IBDZrl1FUtEWrkFTfOE8W4CpEmywhfn1d/T5UYaz6/U9Sh0HdgfHvoHAn1QAAKnlMdVkurzyAvrB+wHK5lyZqrzUVZ32gQBEvtCdwSCERzjuaxQ04DOA2kayeDmcflRBwFgMjcftRzxQT3cN+25jHtSndeQ== 41606834+williamchaseling@users.noreply.github.com>
2018-09-11 22:19:34 +1000

    3 arg check was printing each element individually instead of the particular element it was meant to.

--- src/main.py
+++ src/main.py
@@ -1,31 +1,28 @@
 if __name__ == '__main__':
     import json
-    import os
     import sys
 
     from helpers.print_cli_options_error import print_cli_options_error
     from pylarm import pylarm
 
-    # opens and converts `cli_options.json` into a dictionary
-    with open('./cli_options.json') as data:
+    # Run the arguments passed
+    with open('./cli_options.json') as data:  # . represents the current directory.
         arguments = json.load(data)
 
     args_passed = len(sys.argv)
 
     if args_passed == 1:
-        src_path = os.path.dirname(os.path.realpath(__file__))
-        pylarm(src_path)
+        pylarm('./')
 
     elif args_passed == 2:
         try:
             print(arguments['2'][sys.argv[1]]['info'])
-
         except KeyError:
             print_cli_options_error(sys.argv)
 
     elif args_passed == 3:
         try:
-            print(arguments['3'], [sys.argv[2]], [sys.argv[1]], ['info'])
+            print(arguments['3'][sys.argv[2]][sys.argv[1]]['info'])
 
         except KeyError:
             print_cli_options_error(sys.argv)
