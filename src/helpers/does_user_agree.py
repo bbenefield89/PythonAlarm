3eaa5537b2a4cd5aa73adce4d21637b1b05df441
williamchaseling <ssh-rsa AAAAB3NzaC1yc2EAAAADAQABAAACAQDzRccaHCXyKxoMwVr++5PuaY2weyPoBKI8bydsFJqH7W10uLuIDgCyi8SB6iSfVh03MNQ1nMXCkm3X/RoOW5WTFtWf4X/nbfofMuQwSftdbVUSo8FKvXFvzYEY7gfcgO893YCHNoD6tKaEB2SYEkc1cbfxj9lKfDGh3k7yx7NU1SOwI4nEc68hQ+SmUjtBKfLGi98dD/XaLxt6NLnTqtUEKCULaMd2A1uCD2Sa2WckZbCoLz9DomtLNFQFzgIahdz4TtB4HVI15GRosOSCI4zNU2+b+t6QPcI3SRAvCvriqrfzb2rceSJVx+L2Hcq/owj9nIX9VJDkPXn8tUW/mvFdZId2T1YbIF24DKY5V6UguJCU6oEFTIc1K/o6erSEC7U3eG1KFH06LMUG2Oh2Q+oatsH4690HEiRvQP4AGnDwATKOZAxO3J+AD0mOpmAb374y363hn6eqMArYwul2Afi2aVm9hUAW4EuZ7TtqX8yEa6hlAdDRFRJhB+0PBMQLOEoaIvpSBiU7Bw3Mqf8IBDZrl1FUtEWrkFTfOE8W4CpEmywhfn1d/T5UYaz6/U9Sh0HdgfHvoHAn1QAAKnlMdVkurzyAvrB+wHK5lyZqrzUVZ32gQBEvtCdwSCERzjuaxQ04DOA2kayeDmcflRBwFgMjcftRzxQT3cN+25jHtSndeQ== 41606834+williamchaseling@users.noreply.github.com>
2018-09-12 00:46:57 +1000

    Move the time confirmation into does_user_agree since it was only being called before this function was.

--- src/helpers/does_user_agree.py
+++ src/helpers/does_user_agree.py
@@ -8,7 +8,7 @@ from .manually_set_alarm import manually_set_alarm
 ##
 # does_user_agree
 #
-def does_user_agree(seconds, path):
+def does_user_agree(mk_time, path):
     '''
     @summary    Checks if user input time is correct
 
@@ -21,11 +21,13 @@ def does_user_agree(seconds, path):
     @param      int {seconds}
     @return     {void}
     '''
-    
+
+    print(f'\nThe alarm is set to go off at {time.ctime(mk_time)}\n')
+
     alarm_accepted = check_if_correct()
 
     if alarm_accepted == 'y':
-        print(f'\nThe alarm has been set and will go off at {time.ctime(seconds)}\n')
-        check_alarm(seconds, path)
+        print(f'\nThe alarm has been set and will go off at {time.ctime(mk_time)}\n')
+        check_alarm(mk_time, path)
     elif alarm_accepted == 'n':
         manually_set_alarm()
