d76781f0cb314007fa501311736c95036056c5f1
bbenefield89 <bsquared18@gmail.com>
2018-11-24 18:26:53 -0500

    MOVE PATH APPENDING METHOD CALL TO A MORE APPROPRIATE AREA IN FILE
    - __tests__/test_CLIOptions.py

--- refactor/__tests__/test_CLIOptions.py
+++ refactor/__tests__/test_CLIOptions.py
@@ -1,11 +1,10 @@
 import os
 import sys
 
-sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../pylarm')
-
 from utils.CLIOptions import CLIOptions
 
 
+sys.path.append(os.path.dirname(os.path.realpath(__file__)) + '/../pylarm')
 path_to_cli_options = os.path.dirname(os.path.realpath(__file__)) + '/../pylarm/cli_options.json'
 
 def test_CLIOptions_get_cli_arg_info():
