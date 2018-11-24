eb4b38f17366b63d0ef3c1f1122fcc6d8460007c
bbenefield89 <bsquared18@gmail.com>
2018-11-23 20:59:28 -0500

    REFACTOR LOADING THE CLI OPTIONS FILE INTO ITS OWN FUNCTION FOR INCREASED READABILITY
    - src/helpers/get_cli_options_file.py
    - src/main.py

--- src/main.py
+++ src/main.py
@@ -4,16 +4,12 @@ if __name__ == '__main__':
     import sys
 
     from helpers.print_cli_options_error import print_cli_options_error
+    from helpers.get_cli_options_file import get_cli_options_file
     from pylarm import pylarm
 
 
-    # opens and converts `cli_options.json` into a dictionary
     cli_options_file_path = os.path.dirname(os.path.realpath(__file__)) + '/cli_options.json'
-
-    with open(cli_options_file_path) as data:
-        possible_cli_arguments = json.load(data)
-
-
+    possible_cli_arguments = get_cli_options_file(cli_options_file_path)
     cli_arguments = sys.argv
     number_of_cli_arguments = len(cli_arguments)
     path_to_src_folder = os.path.dirname(os.path.realpath(__file__))
@@ -28,9 +24,9 @@ if __name__ == '__main__':
                 print(possible_cli_arguments['2'][cli_arguments[1]]['info'])
         
             else:
-                s = cli_arguments[1].split(":")
+                alarm_time_given = cli_arguments[1].split(":")
               
-                if len(s) == 2 and s[0].isdigit() and s[1].isdigit():
+                if len(alarm_time_given) == 2 and alarm_time_given[0].isdigit() and alarm_time_given[1].isdigit():
                     pylarm(path_to_src_folder, given_time=cli_arguments[1])
                     
                 else:
